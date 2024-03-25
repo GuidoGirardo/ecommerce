b11f622445df587631dfaa699ec26e179df71079
guido girardo <guidogirardo@gmail.com>
2024-03-25 14:21:23 -0300

    login and register

---
+++ app/src/main/java/com/damaris/ecommerce_vyza/RegisterScreen.kt
@@ -0,0 +1,86 @@
+package com.damaris.ecommerce_vyza
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.damaris.ecommerce_vyza.ui.theme.EcommercevyzaTheme
+import com.damaris.ecommerce_vyza.ui.theme.Rounded8
+
+@Preview
+@Composable
+fun RegisterScreen() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 32.dp, vertical = 16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        OutlinedTextField(
+            value = "",
+            onValueChange = { },
+            modifier = Modifier.fillMaxWidth(),
+            shape = Rounded8,
+            placeholder = {
+                Text("Email")
+            }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = "",
+            onValueChange = { },
+            modifier = Modifier.fillMaxWidth(),
+            shape = Rounded8,
+            placeholder = {
+                Text("Password")
+            }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = "",
+            onValueChange = { },
+            modifier = Modifier.fillMaxWidth(),
+            shape = Rounded8,
+            placeholder = {
+                Text("Confirm Password")
+            }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = { /*TODO*/ },
+            shape = Rounded8
+        ) {
+            Text(text = stringResource(id = R.string.rs_register))
+        }
+        Spacer(modifier = Modifier.height(32.dp))
+        Text(
+            text = stringResource(id = R.string.rs_have_an_account),
+            modifier = Modifier
+                .clickable { }
+        )
+
+    }
+}
---
+++ app/src/main/java/com/damaris/ecommerce_vyza/ui/theme/StylesAndShapes.kt
@@ -0,0 +1,6 @@
+package com.damaris.ecommerce_vyza.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+val Rounded8 = RoundedCornerShape(8.dp)
