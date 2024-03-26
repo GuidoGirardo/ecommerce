24afc2469edb621f6be0b18bc96b9d5b2c425815
guido girardo <guidogirardo@gmail.com>
2024-03-26 00:26:29 -0300

    colors básicos

--- app/src/main/java/com/damaris/ecommerce_vyza/ui/theme/Color.kt
+++ app/src/main/java/com/damaris/ecommerce_vyza/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// colors ecommerce
+val backgroundApp = Color(0xFFF1F1F1)
+val appText = Color(0xFF0a0a0a)
+val grayApp = Color(0xFFafafaf)
+val lightGrayApp = Color(0xFFdfdfdf)
+val colorApp = Color(0xFFFFB114)
