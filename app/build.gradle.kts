172848d62f8505ce0bbb6184b5e3f3ae4940b9ea
ModdyDev <romeroeinsten@gmail.com>
2024-03-25 19:05:30 -0500

    added: Coil

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.io.coil.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
---
+++ app/src/main/java/com/damaris/ecommerce_vyza/ui/components/ProfilePhoto.kt
@@ -0,0 +1,8 @@
+package com.damaris.ecommerce_vyza.ui.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ProfilePhoto() {
+    
+}
